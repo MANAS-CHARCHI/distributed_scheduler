1d64704d06f4fa727b4d5eef557bf175ccd6588e
Manas Charchi <charchimanas2018@gmail.com>
2026-03-16 00:10:05 +0530

    all model design  and configuration

---
+++ database/db.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+engine=create_engine(os.getenv("DATABASE_URL"))
+print(os.getenv("DATABASE_URL"))
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base=declarative_base()
+
+def db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close(
+        
+    )
---
+++ database/models.py
@@ -0,0 +1,94 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime, UTC
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean, JSON, Enum
+from sqlalchemy.ext.declarative import declarative_base
+import uuid, enum
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID, ARRAY
+from database.db import Base
+
+class ScheduleType(str, enum.Enum):
+    ONCE = "once"
+    DAILY = "daily"
+    WEEKLY = "weekly"
+    MONTHLY = "monthly"
+
+class DayOfWeek(int, enum.Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+class SchedulerWebhookData(Base):
+    __tablename__ = "scheduler_webhook_data"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=True, index=True)
+    webhook_url = Column(String(255), nullable=False)
+    webhook_headers = Column(JSON, nullable=True, default=lambda: {})
+    webhook_timeout_seconds=Column(Integer, nullable=False, default=90)
+
+
+class SchedulerCallbackData(Base):
+    __tablename__ = "scheduler_callback_data"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=True, index=True)
+    callback_url = Column(String(255), nullable=True)
+    callback_headers = Column(JSON, nullable=True, default=lambda: {})
+    callback_timeout_seconds=Column(Integer, nullable=False, default=90)
+
+
+class Scheduler(Base):
+    __tablename__ = "scheduler"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=True)
+    webhook_id = Column(Integer, ForeignKey("scheduler_webhook_data.id"), nullable=False)
+    callback_id = Column(Integer, ForeignKey("scheduler_callback_data.id"), nullable=True)
+    webhook = relationship("SchedulerWebhookData")
+    callback = relationship("SchedulerCallbackData")
+    payload = Column(JSON, nullable=False)
+    execution_time = Column(DateTime(timezone=True), nullable=True)
+    schedule_type = Column(Enum(ScheduleType, name="schedule_type_enum"), nullable=False, default=ScheduleType.ONCE)
+    # -> weekly -> [1,3,5]  (Mon Wed Fri) or monthly -> [1,15]
+    repeat_on = Column(ARRAY(Integer), nullable=True)
+    active = Column(Boolean, default=True)
+    max_retry = Column(Integer, default=3)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(UTC))
+    last_executed_at = Column(DateTime(timezone=True), nullable=True)
+    next_executed_at = Column(DateTime(timezone=True), nullable=True, index=True)
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(UTC), onupdate= lambda: datetime.now(UTC))
+
+class ScheduleTaskExecutions(Base):
+    __tablename__ = "schedule_task_executions"
+    id = Column(Integer, primary_key=True)
+    scheduler_id=Column(Integer, ForeignKey("scheduler.id", ondelete="CASCADE"), nullable=False)
+    status = Column(String(10), nullable=False)
+    attempt = Column(Integer, nullable=False)
+    response_code = Column(Integer, nullable=False)
+    executed_at = Column(DateTime(timezone=True), nullable=False)
+    response_time_ms = Column(Integer, nullable=False)
+    error_message = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(UTC))
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(UTC), onupdate= lambda: datetime.now(UTC))
+
+class ScheduleCallbackExecutions(Base):
+    __tablename__ = "schedule_callback_executions"
+    id = Column(Integer, primary_key=True)
+    scheduler_id=Column(Integer, ForeignKey("scheduler.id", ondelete="CASCADE"), nullable=False)
+    status = Column(String(100), nullable=False)
+    attempt = Column(Integer, nullable=False)
+    response_code = Column(Integer, nullable=False)
+    executed_at = Column(DateTime(timezone=True), nullable=False)
+    response_time_ms = Column(Integer, nullable=False)
+    error_message = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(UTC))
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(UTC), onupdate= lambda: datetime.now(UTC))
+
+class ParentScheduler(Base):
+    __tablename__ = "parent_scheduler"
+    id = Column(Integer, primary_key=True)
+    user_id=Column(UUID(as_uuid=True), nullable=True)
+    scheduler_id=Column(Integer, ForeignKey("scheduler.id", ondelete="CASCADE"), nullable=False, index=True)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(UTC))
